edfd467b67074def4b8360887022bcaa9d252d38
Jerome Eberhardt <qksoneo@gmail.com>
2020-10-01 02:10:13 -0700

    fixed boost library location

--- build/python/setup.py
+++ build/python/setup.py
@@ -49,10 +49,10 @@ def find_version():
                 version_match = re.match(r'^__version__ = "(.+?)"$', line)
                 if version_match:
                     return version_match.group(1)
-            raise Exception('Could not find version string in vina/__init__.py.')
+            raise RuntimeError('Could not find version string in vina/__init__.py.')
     except IOError:
         #return '1.2'
-        raise Exception('Could not find vina/__init__.py.')
+        raise RuntimeError('Could not find vina/__init__.py.')
 
 
 def execute_command(cmd_line):
@@ -66,7 +66,8 @@ def execute_command(cmd_line):
 def locate_ob():
     """Try use pkgconfig to locate Open Babel, otherwise guess default location."""
     # Warn if the (major, minor) version of the installed OB doesn't match these python bindings
-    assert is_package_installed("openbabel"), print("Error: Openbabel is not installed.")
+    if not is_package_installed("openbabel"):
+        raise RuntimeError("Error: Openbabel is not installed.")
 
     py_ver = StrictVersion(find_package_version('openbabel'))
     py_major_ver, py_minor_ver = py_ver.version[:2]
@@ -78,7 +79,7 @@ def locate_ob():
         library_dirs = data_pathname + os.path.sep + 'lib'
 
         if os.path.isdir(include_dirs):
-            print('Open Babel location automatically determined in Anaconda:')
+            print('Open Babel location automatically determined in Anaconda.')
             return include_dirs, library_dirs
         else:
             print("Warning: We are in an Anaconda env, but Openbabel is not installed here.")
@@ -96,7 +97,7 @@ def locate_ob():
         include_dirs = execute_command("pkg-config --variable=pkgincludedir %s" % pcfile)[0].strip()
         library_dirs = execute_command("pkg-config --variable=libdir %s" % pcfile)[0].strip()
 
-        print('Open Babel location automatically determined by pkg-config:')
+        print('Open Babel location automatically determined by pkg-config.')
         return include_dirs, library_dirs
     else:
         pathnames = ['/usr', '/usr/local']
@@ -105,14 +106,38 @@ def locate_ob():
             library_dirs = pathname + os.path.sep + 'lib'
 
             if os.path.isdir(include_dirs):
-                print('Open Babel location was automatically guessed:')
+                print('Open Babel location was automatically guessed.')
                 return include_dirs, library_dirs
 
-        print('Open Babel location was set to default location:')
+        print('Open Babel location was set to default location.')
         return include_dirs, library_dirs
 
 
 def locate_boost():
+    """Try to locate boost."""
+    if in_conda:
+        # It means that Openbabel was installed in an Anaconda env
+        data_pathname = sysconfig.get_path('data')
+        include_dirs = data_pathname + os.path.sep + 'include' + os.path.sep + 'boost'
+        library_dirs = data_pathname + os.path.sep + 'lib'
+
+        if os.path.isdir(include_dirs):
+            print('Boost library location automatically determined in Anaconda.')
+            return include_dirs, library_dirs
+    else:
+        include_dirs = '/usr/include' + os.path.sep + 'boost'
+
+        if os.path.isdir(include_dirs):
+            print('Boost library location was automatically guessed.')
+
+            if glob.glob('usr/lib/x86_64-linux-gnu/libboost*.so'):
+                return include_dirs, 'usr/lib/x86_64-linux-gnu'
+            elif glob.glob('usr/lib64/libboost*.so'):
+                return include_dirs, 'usr/lib64'
+            elif glob.glob('usr/lib/libboost*.so'):
+                return include_dirs, 'usr/lib'
+
+    print('Boost library location was set to default location.')
     return "/usr/local/include", "/usr/local/lib"
 
 
@@ -145,7 +170,7 @@ class CustomBuildExt(build_ext):
         # Setting include_dirs, library_dirs, swig_opts here instead of in Extension constructor allows them to be
         # overridden using -I and -L command line options to python setup.py build_ext.
         build_ext.finalize_options(self)
-        
+
         # Boost
         self.boost_include_dir, self.boost_library_dir = locate_boost()
         self.include_dirs.append(self.boost_include_dir)
@@ -159,9 +184,9 @@ class CustomBuildExt(build_ext):
         # SWIG
         # shadow, creates a pythonic wrapper around vina
         # castmode
-        self.swig_opts = ['-c++', '-small', '-naturalvar', '-fastdispatch', '-shadow', '-py3', '-threads']
+        self.swig_opts = ['-c++', '-small', '-naturalvar', '-fastdispatch', '-shadow', '-py3']
         self.swig_opts += ['-I%s' % i for i in self.include_dirs]
-        
+
         print('- include_dirs: %s\n- library_dirs: %s' % (self.include_dirs, self.library_dirs))
         print('- swig options: %s' % self.swig_opts)
         print('- libraries: %s' % self.libraries)
@@ -199,9 +224,8 @@ class CustomBuildExt(build_ext):
 obextension = Extension(
     'vina._vina_wrapper',
     sources=glob.glob('../../src/lib/*.cpp') + ['vina/autodock_vina.i'],
-    extra_link_args=['-lboost_system', '-lboost_thread', 
-                     '-lboost_serialization', '-lboost_filesystem', 
-                     '-lboost_program_options'],
+    extra_link_args=['-lboost_thread', '-lboost_serialization',
+                     '-lboost_filesystem', '-lboost_program_options'],
     libraries=['openbabel'],
     headers=glob.glob('../../src/lib/*.h'),
 )
@@ -210,8 +234,8 @@ obextension = Extension(
 setup(
     name='vina',
     version=find_version(),
-    author='The beautiful people',
-    author_email='people@scripps.edu',
+    author='Diogo Santos Martins, Jerome Eberhardt, Andreas F. Tillack, Stefano forli',
+    author_email='forli@scripps.edu',
     license='Apache-2.0',
     url='https://ccsb.scripps.edu/',
     description='Python interface to AutoDock Vina',
